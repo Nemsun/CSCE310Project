8f622ef587a644130199e1bbbd8535d8561546f3
Namson Pham <namsonpham@gmail.com>
2023-11-29 00:09:00 -0600

    add events button functional TODO: error handling

--- includes/process_event.php
+++ includes/process_event.php
@@ -5,5 +5,45 @@ include_once 'dbh.inc.php';
 $sql = "SELECT * FROM event;";
 $result = mysqli_query($conn, $sql);
 
+if (isset($_POST['add_btn'])) {
+    $uin = $_POST['uin'];
+    $programNum = $_POST['program_num'];
+    $startDate = $_POST['start_date'];
+    $startTime = $_POST['start_time'];
+    $location = $_POST['location'];
+    $endDate = $_POST['end_date'];
+    $endTime = $_POST['end_time'];
+    $eventType = $_POST['event_type'];
 
+    // TODO: Error Checking
+    // ***Start date is before end date
+    $startDateTime = new DateTime($startDate . ' ' . $startTime);
+    $endDateTime = new DateTime($endDate . ' ' . $endTime);
+    if ($startDateTime >= $endDateTime) {
+        header("Location: ../pages/event_admin.php?error=startdateafterenddate");
+        echo "Start date is after end date";
+        exit();
+    }
+    // ***Program number should be between 1-5 (FK)
+    if ($programNum < 1 || $programNum > 5) {
+        header("Location: ../pages/event_admin.php?error=invalidprogramnum");
+        echo "Program number should be between 1-5";
+        exit();
+    }
+    // Database insertion
+    $stmt = $conn->prepare("INSERT INTO event (UIN, Program_Num, Start_Date, Start_Time, Location, End_Date, End_Time, Event_Type) 
+                            VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("iissssss", $uin, $programNum, $startDate, $startTime, $location, $endDate, $endTime, $eventType);
+    $stmt->execute();
+
+    $stmt->close();
+    $conn->close();
+
+    header("Location: ../pages/event_admin.php?addevent=success");
+    exit();
+
+} else {
+    header("Location: error.php");
+    exit();
+}
 ?>
--- pages/event_admin.php
+++ pages/event_admin.php
@@ -11,15 +11,14 @@
                 <h3>Add Event</h3>
                 <button autofocus id="close-event-modal" class="close-modal-btn">&times;</button>
             </div>
-            <form>
-                <input type="text" placeholder="Event ID" name="event_id" required>
+            <form action="../includes/process_event.php" method="post">
                 <input type="text" placeholder="UIN" name="uin" required>
-                <input type="text" placeholder="Program Number" name="program_num" required>
-                <input type="text" placeholder="Start Date" name="start_date" required>
-                <input type="text" placeholder="Start Time" name="start_time" required>
+                <input type="text" placeholder="Program Number (1-5)" name="program_num" required>
+                <input type="text" placeholder="Start Date (yyyy-mm-dd)" name="start_date" required>
+                <input type="text" placeholder="Start Time (00:00:00)" name="start_time" required>
                 <input type="text" placeholder="Location" name="location" required>
-                <input type="text" placeholder="End Date" name="end_date" required>
-                <input type="text" placeholder="End Time" name="end_time" required>
+                <input type="text" placeholder="End Date (yyyy-mm-dd)" name="end_date" required>
+                <input type="text" placeholder="End Time (00:00:00)" name="end_time" required>
                 <input type="text" placeholder="Event Type" name="event_type" required>
                 <button type="submit" class="add-btn center margin-top" name="add_btn">Add</button>
             </form>
