3b0f2c3dda2b834aa7616bc16f27d81441914488
Namson Pham <namsonpham@gmail.com>
2023-12-03 16:49:09 -0600

    first commit

--- pages/application_information.php
+++ pages/application_information.php
@@ -1,4 +1,9 @@
 <?php include '../assets/header.php'; 
 include '../assets/student_navbar.php'; 
 include_once '../includes/dbh.inc.php'; 
-session_start(); 
+session_start();
+?>
+
+<div class="main-container">
+    <h2> Welcome back <?php echo $_SESSION['first_name']; ?>!</h2>
+</div>
