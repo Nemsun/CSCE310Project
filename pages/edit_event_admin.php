5adc7fd1f19d1afadc50480c89d6f8a91ecab5f0
Namson Pham <namsonpham@gmail.com>
2023-12-03 16:35:04 -0600

    events: 90% finished, TODO: check if program number exists

--- pages/edit_event_admin.php
+++ pages/edit_event_admin.php
@@ -4,43 +4,52 @@ include_once '../includes/dbh.inc.php';
 session_start(); ?>
 
 <div class="main-container">
-        <?php
-            if(isset($_SESSION['success'])) {
-                echo '<div class="alert alert-success" role="alert" id="alert">' . $_SESSION['success'] . '<span class="alert-close-btn" onclick="closeAlert()">&times;</span>' . '</div>';
-                unset($_SESSION['success']);
-            } else if(isset($_SESSION['error'])) {
-                echo '<div class="alert alert-danger" role="alert" id="alert">' . $_SESSION['error'] . '<span class="alert-close-btn" onclick="closeAlert()">&times;</span>' . '</div>';
-                unset($_SESSION['error']);
-            }
-        ?>
     <div class="header">
         <h2>Edit Event</h2>
     </div>
-    <form class="edit-form flex flex-col flex-start align-start" action="#" method="post">
-        <label class="event-label text-black font-size-l pd-10" for="uin-id">UIN</label>
-        <input class="pd-20 border-radius-12 edit-input" id="uin-id" type="text" placeholder="UIN" name="UIN">
+    <?php
+        if (isset($_POST['edit_btn'])) {
+            $eventEditID = $_POST['edit_id'];
+            $stmt = $conn->prepare("SELECT * FROM event WHERE Event_Id = ?");
+            $stmt->bind_param("i", $eventEditID);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $stmt->close();
+            foreach ($result as $row) {
+            ?>
+            <form class="edit-form flex flex-col flex-start align-start" action="../includes/process_edit_event.php" method="post">
+                <input type="hidden" name="edit_id" value="<?php echo $row['Event_Id']; ?>">
+                <label class="event-label text-black font-size-l pd-10" for="uin-id">UIN</label>
+                <input class="pd-20 border-radius-12 edit-input" id="uin-id" type="text" placeholder="UIN" name="edit_UIN" value="<?php echo $row['UIN']?>">
 
-        <label class="event-label text-black font-size-l pd-10" for="program-num">Program Number</label>
-        <input class="pd-20 border-radius-12 edit-input" id="program-num" type="text" placeholder="Program Number (1-5)" name="program_num">
-        
-        <label class="event-label text-black font-size-l pd-10" for="start-date">Start Date</label>
-        <input class="pd-20 border-radius-12 edit-input" id="start-date" type="date" name="start_date">
+                <label class="event-label text-black font-size-l pd-10" for="program-num">Program Number</label>
+                <input class="pd-20 border-radius-12 edit-input" id="program-num" type="text" placeholder="Program Number (1-5)" name="edit_program_num" value="<?php echo $row['Program_Num']?>">
+                
+                <label class="event-label text-black font-size-l pd-10" for="start-date">Start Date</label>
+                <input class="pd-20 border-radius-12 edit-input" id="start-date" type="date" name="edit_start_date" value="<?php echo $row['Start_Date']?>">
 
-        <label class="event-label text-black font-size-l pd-10" for="start-time">Start Time</label>
-        <input class="pd-20 border-radius-12 edit-input" id="start-time" type="time" name="start_time">
+                <label class="event-label text-black font-size-l pd-10" for="start-time">Start Time</label>
+                <input class="pd-20 border-radius-12 edit-input" id="start-time" type="time" name="edit_start_time" value="<?php echo $row['Start_Time']?>">
 
-        <label class="event-label text-black font-size-l pd-10" for="location-id">Location</label>
-        <input class="pd-20 border-radius-12 edit-input" id="location-id" type="text" placeholder="Location" name="location">
+                <label class="event-label text-black font-size-l pd-10" for="location-id">Location</label>
+                <input class="pd-20 border-radius-12 edit-input" id="location-id" type="text" placeholder="Location" name="edit_location" value="<?php echo $row['Location']?>">
 
-        <label class="event-label text-black font-size-l pd-10" for="end-date">End Date</label>
-        <input class="pd-20 border-radius-12 edit-input" id="end-date" type="date" name="end_date">
+                <label class="event-label text-black font-size-l pd-10" for="end-date">End Date</label>
+                <input class="pd-20 border-radius-12 edit-input" id="end-date" type="date" name="edit_end_date" value="<?php echo $row['End_Date']?>">
 
-        <label class="event-label text-black font-size-l pd-10" for="end-time">End Time</label>
-        <input class="pd-20 border-radius-12 edit-input" id="end-time" type="time" name="end_time">
+                <label class="event-label text-black font-size-l pd-10" for="end-time">End Time</label>
+                <input class="pd-20 border-radius-12 edit-input" id="end-time" type="time" name="edit_end_time" value="<?php echo $row['End_Time']?>">
 
-        <label class="event-label text-black font-size-l pd-10" for="event-type">Event Type</label>
-        <input class="pd-20 border-radius-12 edit-input" id="event-type" type="text" placeholder="Event Type" name="event_type">
-        
-        <button type="submit" class="add-btn margin-top-20" name="edit_btn">Update</button>
-    </form>
+                <label class="event-label text-black font-size-l pd-10" for="event-type">Event Type</label>
+                <input class="pd-20 border-radius-12 edit-input" id="event-type" type="text" placeholder="Event Type" name="edit_event_type" value="<?php echo $row['Event_Type']?>">
+                
+                <div class="flex space-between width-50">
+                    <button type="submit" class="add-btn margin-top-20" name="update_btn">Update</button>
+                    <a href="event_admin.php" class="cancel-btn margin-top-20">Cancel</a>
+                </div>
+            </form>
+        <?php 
+            } 
+        } 
+    ?>
 </div>
