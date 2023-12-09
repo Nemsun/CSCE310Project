706c7fd498688b660b7e3a57e3b218b190dc6af8
Namson Pham <namsonpham@gmail.com>
2023-12-08 18:31:48 -0600

    db tweak

--- cybersecdata.sql
+++ cybersecdata.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `cybersecdata`
 --
-
+CREATE DATABASE IF NOT EXISTS `cybersecdata` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `cybersecdata`;
 -- --------------------------------------------------------
 
 --
